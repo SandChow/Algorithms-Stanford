e60598540bd61a367488d38134d3b58144e71662
SandChow <srisandeep@hotmail.com>
2016-11-26 04:15:34 -0800

    Update variable names

--- karatsuba.py
+++ karatsuba.py
@@ -3,12 +3,11 @@ def karatsuba(num1, num2):
 		return num1 * num2
 
 	# calculates the size of the numbers
-	m = max(len(str(num1)), len(str(num2)))
-	m2 = m / 2
+	maxLength = max(len(str(num1)), len(str(num2)))
 
 	# split the digit sequences about the middle
-	high1, low1 = num1 / 10**(m2), num1 % 10**(m2)
-	high2, low2 = num2 / 10**(m2), num2 % 10**(m2)
+	high1, low1 = num1 / 10**(maxLength/2), num1 % 10**(maxLength/2)
+	high2, low2 = num2 / 10**(maxLength/2), num2 % 10**(maxLength/2)
 
 	# debug
 	print "num1 -> ",; print high1, low1 
@@ -19,7 +18,7 @@ def karatsuba(num1, num2):
 	z0 = karatsuba(low1, low2)
 	z1 = karatsuba(low1+high1, low2+high2)
 	z2 = karatsuba(high1, high2)
-	return z2 * 10**(2*m2) + (z1-z2-z0) * 10**m2 + z0
+	return z2 * 10**(2*maxLength/2) + (z1-z2-z0) * 10**(maxLength/2) + z0
 
 if __name__=="__main__":
 	print karatsuba(2400000, 52)
