d35e9432d520a8cfab428369cdd25c76ecb992fe
SandChow <srisandeep@hotmail.com>
2016-12-03 00:11:40 -0800

    implement all basic functions

--- data-structures/graph/Graph.py
+++ data-structures/graph/Graph.py
@@ -36,3 +36,25 @@ class Graph(object):
                 del self._graph[node]
             except KeyError:
                 pass
+
+    def are_connected(self, node1, node2):
+        """ Checks whether two nodes are connected to each other 
+            regardless of whether the graph is directed """
+
+        if node1 in self._graph and node2 in self._graph:
+            return node1 in self._graph[node2] or node2 in self._graph[node1]
+        return False
+
+    def find_path(self, node1, node2, path = []):
+        """ Finds any path from node1 to node2 which may not be the shortest """
+        current_path = path + [node1]
+        if node1 == node2:
+            return current_path
+        if node1 not in self._graph:
+            return None
+        for node in self._graph[node1]:
+            if node not in current_path:
+                new_path = self.find_path(node, node2, current_path)
+                if new_path:
+                    return new_path
+        return None
--- data-structures/graph/main.py
+++ data-structures/graph/main.py
@@ -5,5 +5,5 @@ if __name__=="__main__":
     connections = [('A', 'B'), ('B', 'C'), ('B', 'D'), ('C', 'D'), ('E', 'F'), ('F', 'C')]
     x = Graph(connections)
     print x
-    x.remove('B')
-    print x
+    print x.are_connected('B', 'C')
+    print x.find_path('C', 'D')
