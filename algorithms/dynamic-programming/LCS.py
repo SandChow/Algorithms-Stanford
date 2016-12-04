dbaa423187a47f067a97511e4d74e73274ca5ff4
SandChow <srisandeep@hotmail.com>
2016-12-03 19:53:29 -0800

    debug LCS

--- algorithms/dynamic-programming/LCS.py
+++ algorithms/dynamic-programming/LCS.py
@@ -5,8 +5,7 @@ Implementation of longest common subsequence (LCS) algorithm.
 def lcs(A, B):
     a = len(A)
     b = len(B)
-    D = [[None]*(b+1) for i in xrange(a+1)]
-
+    D = [[0]*(b+1) for i in xrange(a+1)]
     for i in xrange(a+1):
         for j in xrange(b+1):
             if j == 0 or i == 0:
@@ -15,8 +14,8 @@ def lcs(A, B):
                 D[i][j] = D[i-1][j-1] + 1;
             else:
                 D[i][j] = max(D[i][j-1], D[i-1][j])
-    print D[a][b]
+    return D[a][b]
 
 if __name__ == '__main__':
-    lcs("ABCD", "ABDCD")
-    lcs("ABCBDAB", "BDCABCABCBDAB")
+    print lcs("ABCD", "ABDCD")
+    print lcs("ABCBDAB", "BDCABCABCBDAB")
