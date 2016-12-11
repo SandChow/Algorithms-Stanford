b34fc99148ac728e7bf6f803c1b1a4bd19598ff3
SandChow <srisandeep@hotmail.com>
2016-12-10 19:27:57 -0800

    add dfs

--- data-structures/graph/main.py
+++ data-structures/graph/main.py
@@ -2,9 +2,8 @@ from Graph import Graph
 from pprint import pprint
 
 if __name__=="__main__":
-    connections = [('A', 'B'), ('B', 'C'), ('B', 'D'), ('C', 'D'), ('E', 'F'), ('F', 'C'), ('D', 'E')]
+    connections = [('A', 'B'), ('B', 'C'), ('B', 'D'), ('C', 'D'), ('E', 'F'), ('F', 'C')]
     x = Graph(connections)
-    #print x
-    #print x.are_connected('B', 'C')
-    #print x.find_path('C', 'D')
-    print x.dfs("A")
+    print x
+    print x.are_connected('B', 'C')
+    print x.find_path('C', 'D')
